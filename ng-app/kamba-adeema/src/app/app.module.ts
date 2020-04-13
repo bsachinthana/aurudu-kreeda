b8dc3047f0afae740e5805de503971259490716e
Charith Abeygunawardhana <charith9422@gmail.com>
2020-04-13 17:30:51 +0530

    Login game UI added

--- ng-app/kamba-adeema/src/app/app.module.ts
+++ ng-app/kamba-adeema/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './layout/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './layout/material/material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeComponent } from './layout/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
