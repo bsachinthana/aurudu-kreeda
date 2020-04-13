d38f45f6b4b5f5c1f5652b3ba6e97e5b7734ffeb
malithkalhara <kadikiwihari@gmail.com>
2020-04-13 23:56:00 +0530

    Made separate canvas component. Changes are done to the app.component.

--- ng-app/kamba-adeema/src/app/app.component.ts
+++ ng-app/kamba-adeema/src/app/app.component.ts
@@ -9,48 +9,10 @@ import { Square } from './square';
 export class AppComponent implements OnInit {
   title = 'kamba-adeema';
 
-  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
-  ctx: CanvasRenderingContext2D;
-  requestId;
-  interval;
-  squares: Square[] = [];
-
-  constructor(private ngZone: NgZone) {}
+  constructor() {}
 
   ngOnInit() {
-    this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.ctx.fillStyle = 'red';
-    this.ngZone.runOutsideAngular(() => this.tick());
-    setInterval(() => {
-      this.tick();
-    }, 200);
-  }
-
-  tick() {
-    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.squares.forEach((square: Square) => {
-      square.moveRight();
-    });
-    this.requestId = requestAnimationFrame(() => this.tick);
-  }
-
-  playRight(){
-    const square = new Square(this.ctx);
-    this.squares = this.squares.concat(square);
-    this.squares.forEach((square: Square) => {
-      square.moveLeft();
-    });
-    this.requestId = requestAnimationFrame(() => this.tick);
-  }
-
-  playLeft() {
-    const square = new Square(this.ctx);
-    this.squares = this.squares.concat(square);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
-    cancelAnimationFrame(this.requestId);
+    
   }
 
 }
