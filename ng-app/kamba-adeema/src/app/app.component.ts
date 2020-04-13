420e9f7bb45f1fa9fbb51a9b959132166e5cac28
malithkalhara <kadikiwihari@gmail.com>
2020-04-13 21:44:11 +0530

    Initial Commit for canvas integration. Right button's functionality should be little bit changed.

--- ng-app/kamba-adeema/src/app/app.component.ts
+++ ng-app/kamba-adeema/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { Square } from './square';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kamba-adeema';
+
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+  requestId;
+  interval;
+  squares: Square[] = [];
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = 'red';
+    this.ngZone.runOutsideAngular(() => this.tick());
+    setInterval(() => {
+      this.tick();
+    }, 200);
+  }
+
+  tick() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.squares.forEach((square: Square) => {
+      square.moveRight();
+    });
+    this.requestId = requestAnimationFrame(() => this.tick);
+  }
+
+  playRight(){
+    const square = new Square(this.ctx);
+    this.squares = this.squares.concat(square);
+    this.squares.forEach((square: Square) => {
+      square.moveLeft();
+    });
+    this.requestId = requestAnimationFrame(() => this.tick);
+  }
+
+  playLeft() {
+    const square = new Square(this.ctx);
+    this.squares = this.squares.concat(square);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    cancelAnimationFrame(this.requestId);
+  }
+
 }
